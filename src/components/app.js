a5065accaf7799fd8745bd5caa5b7e07fcb8bd5b
dbr408 <daniel@jbfam.me>
2019-07-17 09:11:17 -0600

    why

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 import PortfolioContainer from './portfolio/portfolio-container';
-import navigationContainer from './navigation/navigationcontainer';
+import NavigationContainer from './navigation/navigationcontainer';
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <navigationContainer
-        ><h1>Why Why React</h1> ;
+        <Router>
+          <div>
+           <NavigationContainer />
+
+           <Switch>
+            <Route exact path="/" Component={Home} />
+            <Route path="/about-me" Component={About} />
+            <Route path="/contact" Component={Contact} />
+            <Route path="/blog" Component={Blog} />
+           </Switch>
+          </div>
+        </Router>
+
+        <h1>Why Why React</h1> ;
         < PortfolioContainer />
         {moment().format('MMMM Do YYYY, h:mm:ss a')}
         </div>
--- src/components/navigation/navigationcontainer.js
+++ src/components/navigation/navigationcontainer.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
+import { Navlink } from "react-router-dom";
 
 export default class navigationComponent extends Component {
     constructor() {
       super();
     }
 
-    render() {
-        return (
-            <div>
-               <button>Home</button>
-               <button>About</button>
-               <button>Contact</button>
-               <botton>Blog</botton>
-               <botton>Add Blog</botton>
-            </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+      <NavLink exact to="/">
+        Home
+      </NavLink>
+
+      <Navlink to="/about-me">About</Navlink>
+      <Navlink to="/contact">Contact</Navlink>
+      <Navlink to="/blog">Blog</Navlink>
+      
+        {true ? <botton>Add Blog</botton> : null }
+      </div>
+    );
+  }
 }
