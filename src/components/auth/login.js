1ffc1abe3cb85f03948180dd8e27fe99a1f6eb39
dbr408 <daniel@jbfam.me>
2019-07-19 10:33:10 -0600

    done with that

---
+++ src/components/auth/login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class login extends Component {
+    render() {
+        return(
+        <div>
+          <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+         <form>
+            <input type="text" />
+            <input type="password" />
+         </form>
+        </div>
+        );
+    }
+}
--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import login from "../auth/login"
 import loginImg from "../../../static/assets/images/auth/login.jpg"
 
 export default class Auth extends Component {
@@ -13,7 +14,7 @@ export default class Auth extends Component {
                 />
 
                 <div className="right-column">
-                    <h1>login here</h1>
+                    <login  />
                 </div>
             </div>
         );
