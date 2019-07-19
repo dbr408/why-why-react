d483e77d3164f26feb3b3fcf858e7b988d7e9c08
dbr408 <daniel@jbfam.me>
2019-07-19 10:56:02 -0600

    i forgot

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
 
 export default class login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        console.log("Handle submit", event);
+    }
+
     render() {
         return(
         <div>
           <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
-         <form>
-            <input type="text" />
-            <input type="password" />
+
+         <form onSubmit={this.handleSubmit}>
+            <input 
+            type="email" 
+            name="email"
+            placeholder="Your email"
+            value={this.state.email}
+            onChang={this.handleChange}
+            />
+            <input 
+            type="password" 
+            name="passward"
+            placeholder="Your password"
+            value={this.state.password}
+            onChang={this.handleChange}
+            />
+
+            <div>
+            <button type="submit"></button>
+            </div>
          </form>
         </div>
         );
