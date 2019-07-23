64b2743cdcf18483b4678b51014edc8abd1b11d4
dbr408 <daniel@jbfam.me>
2019-07-23 08:36:44 -0600

    error

--- src/components/portfolio/portfoliofrom.js
+++ src/components/portfolio/portfoliofrom.js
@@ -1,10 +1,89 @@
 import React, {Component} from'react';
 
 export default class PortfolioForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            description: "",
+            category: "",
+            position: "",
+            url: "",
+            thumb_image: "",
+            banner_image: "",
+            logo: "",
+        };
+
+        this.handleChange=this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event) {
+      this.setState({
+          [event.target.name]: event.target.value
+      });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div>
+                <h1></h1>
 
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <input
+                        type="text"
+                        name="name"
+                        placeholder="random"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                        />
+
+                        <input
+                        type="text"
+                        name="url"
+                        placeholder="URL"
+                        value={this.state.url}
+                        onChange={this.handleChange}
+                        />
+                    </div>
+
+                    <div>
+                    <input
+                        type="text"
+                        name="position"
+                        placeholder="position"
+                        value={this.state.position}
+                        onChange={this.handleChange}
+                        />
+                    <input
+                        type="text"
+                        name="category"
+                        placeholder="Category"
+                        value={this.state.category}
+                        onChange={this.handleChange}
+                        />
+                    </div>
+
+                    <div>
+                    <input
+                        type="text"
+                        name="description"
+                        placeholder="description"
+                        value={this.state.discription}
+                        onChange={this.handleChange}
+                        />
+                    </div>
+
+                    <div>
+                        <button type="submit">Save</button>
+                    </div>
+                </form>
             </div>
         )
     }
