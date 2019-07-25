a5789ea83f942a0ed2e1e0d7435718a121719995
dbr408 <daniel@jbfam.me>
2019-07-25 11:56:01 -0600

    commit

---
+++ src/components/pages/blog-detail.js
@@ -0,0 +1,10 @@
+import react, {component} from 'react';
+export default class BlogDetail extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Blog Detail</h1>
+            </div>
+        )
+    }
+}
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios'
+import BlogItem from "../blog/blog-item"
 
-export default function() {
-    return (
-       <div>
-         <h2>Blog</h2>
+class Blog extends Component {
+  constructor() {
+    super();
 
-         <div>
-           <Link to="/about-me">Read more about me</Link>
-         </div>
-       </div>
-    );
+    this.state = {
+      blogItems: []
+    }
+
+    this.getBlogItems = this.getBlogItems.bind(this)
+  }
+
+  getBlogItems() {
+    Axios.get("https://danielbrown.devcamp.space/portfolio/portfolio_blogs", {withCredentials: true 
+  }).then(Response => {
+    this.setstate({
+      blogItems: Response.data.portfolio_blogs
+    })
+  }).catch(error => {
+    console.log("blog", error)
+  })
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <BlogItem key={blofItem.id} blogItem={blogItem} />;
+    })
+    return 
+       <div>{blogRecords}</div>;
+  }
 }
+export default Blog
